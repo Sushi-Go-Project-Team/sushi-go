4e78172c66cee89f94115cd4da0fdbb3a0cba930
unknown <amytang325@gmail.com>
2023-02-05 19:00:14 -0500

    added card component to Game.js file

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-export default function Card() {
+export default function Card(props) {
     return (
         <div>
             <h1>Card</h1>
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,9 +1,11 @@
 import React from "react"
+import Card from "../components/Card.js"
 
 export default function Game() {
     return (
         <div>
             <h1>Game</h1>
+            <Card />
         </div>
     )
 }
