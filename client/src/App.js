b0479d2ded39bf925ba934b61ab9901e58832dad
unknown <amytang325@gmail.com>
2023-02-11 20:24:08 -0500

    added socket io functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import React from "react"
+import io from "socket.io-client"
 import Card from "./components/Card"
 import Game from "./pages/Game"
 import Instructions from "./pages/Instructions"
@@ -6,6 +7,8 @@ import Join from "./pages/Join"
 import Results from "./pages/Results"
 
 export default function App() {
+  const socket = io.connect("http://localhost:4000")
+
   return (
     <div className="App">
       <h1>Hello World</h1>
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const app = express()
+const cors = require('cors')
+app.use(cors())
+app.use(express.json())
+var router = express.Router()
+
+const server = app.listen(4000, () => {
+  console.log('Server is running on port 4000!')
+});
+
+io = require('socket.io')(server, {
+  cors:{
+    origin:"*"
+  }
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected & Socket Id is ", socket.id)
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
