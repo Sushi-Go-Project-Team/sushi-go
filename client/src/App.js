012f9fefc01af3d9affed1d2c993c132a03a9aba
Kunal <kunalmohindra@optonline.net>
2023-02-14 19:07:46 -0500

    Draft of Results Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,9 @@ import Card from "./components/Card"
 import Game from "./pages/Game"
 import Instructions from "./pages/Instructions"
 import Join from "./pages/Join"
-import Results from "./pages/Results"
+import Results from "./pages/Results";
+import '../src/style.css'
+
 
 export default function App() {
   return (
@@ -13,7 +15,7 @@ export default function App() {
       <Game />
       <Instructions />
       <Join />
-      <Results />
+      <Results> </Results>
     </div>
   );
 }
