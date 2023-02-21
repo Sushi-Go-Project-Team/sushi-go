08f3a58971910ee5bf5ba37aeba9e8aa1c8f57f9
Julie Young <julieyoung@lawn-128-61-26-170.lawn.gatech.edu>
2023-02-20 22:26:59 -0500

    Basic formatting for a card.

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -7,6 +7,9 @@ export default function Game() {
             {/* <h1>Game</h1>
             <Card />
             <Card /> */}
+            <h1>Game</h1>
+            <Card name="Soy Sauce Eggs" image="Eggs.jpg" value="14"/>
+            <Card name="Bok Choy" image="Bok Choy.jpg" value="14"/>
         </div>
     )
 }
