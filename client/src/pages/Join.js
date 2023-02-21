237f482bb2559f907ff5011e22c8ee9a9f224a96
Kunal <kunalmohindra@optonline.net>
2023-02-21 18:45:50 -0500

    Updated button and players

--- client/src/pages/Join.js
+++ client/src/pages/Join.js
@@ -1,16 +1,24 @@
 import React from "react"
 
 export default function Join() {
-    return (  
-		<><div class="banner">
-			<h1>Sushi Go</h1>
-		</div>
-		<div class="center">
-			<h2>Join Game</h2>
-			<input type="text" placeholder="Game Pin" />
-			<button class="button">Enter</button>
-			<p>OR</p>
-			<button class="button">Create New</button>
-		</div></>
+    return (
+        <div>
+            <head>
+	            <title>Sushi Go</title>
+	            <link rel="stylesheet" type="text/css" href="style.css"/>
+            </head>
+            <body>
+	            <div class="banner">
+		            <h1>Sushi Go</h1>
+	            </div>
+	            <div class="center">
+		            <h2>Join Game</h2>
+		            <input type="text" placeholder="Game Pin"/>
+		            <button class="button">Enter</button>
+		            <p>OR</p>
+		            <button class="button">Create New</button>
+	            </div>
+            </body>
+        </div>
     )
 }
