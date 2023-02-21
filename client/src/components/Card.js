692b550704cfaeea9e63deba6469c049369e3910
Julie Young <julieyoung@lawn-128-61-26-170.lawn.gatech.edu>
2023-02-20 22:34:33 -0500

    More card changes.

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -5,8 +5,8 @@ export default function Card(props) {
         <div>
             <img src={props.image} className="Card--image"/>
             <img src={props.image} className="Card--image"/>
-            <h1 className="Card--name">props.name</h1>
-            <h1 className="Card--value">props.value</h1>
+            <h1 className="Card--name">{props.name}</h1>
+            <h1 className="Card--value">{props.value}</h1>
         </div>
     )
 }
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -9,7 +9,7 @@ export default function Game() {
             <Card /> */}
             <h1>Game</h1>
             <Card name="Soy Sauce Eggs" image="Eggs.jpg" value="14"/>
-            <Card name="Bok Choy" image="Bok Choy.jpg" value="14"/>
+            <Card name="Bok Choy" image="BokChoy.jpeg" value="14"/>
         </div>
     )
 }
