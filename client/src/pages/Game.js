1acc2ebefae6393ebfb171f1307406934cd55a66
tinan272 <mnguyen354@gatech.edu>
2023-02-25 23:11:21 -0500

    Layout cards on gamepage

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -3,13 +3,44 @@ import Card from "../components/Card.js"
 
 export default function Game() {
     return (
-        <div>
+        <div className="GamePage">
             {/* <h1>Game</h1>
             <Card />
             <Card /> */}
             <h1>Game</h1>
-            <Card name="Soy Sauce Eggs" image="Eggs.jpg" value="14"/>
-            <Card name="Bok Choy" image="Bok Choy.jpg" value="14"/>
+            <div className="Card--container">
+                <div className="Card--setOne">
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <div className="Card--deck">
+                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                    </div>
+                </div>
+                <div className="Card--setTwo">
+                    <div className="Card--deck">
+                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                    </div>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card id="deck2" className="Card--image" title="Deck 2" image="images/GameCards.png" value="14"/>
+                </div>
+                <div className="Card--setThree">
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <div className="Card--deck">
+                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                    </div>                </div>
+                <div className="Card--setFour">
+                    <div className="Card--deck">
+                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                    </div>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card id="deck4" className="Card--image" name="Deck 4" image="images/GameCards.png" value="14"/>
+                </div>
+            </div>
         </div>
     )
 }
