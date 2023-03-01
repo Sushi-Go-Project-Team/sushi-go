96dbff9d2c217a294e6cb4ecbcdc7885480fbd78
Kunal <kunalmohindra@optonline.net>
2023-02-28 19:26:42 -0500

    Created User and Deck classes. Still need to connect deck functions to users

---
+++ client/src/Classes/Deck.js
@@ -0,0 +1,29 @@
+//Have a count for how many cards there is in total
+class Deck {
+    deck = []
+    constructor() {
+        //Hard code cards in deck for now
+        this.deck = [Card("Soy Sauce Eggs", 4), Card("Soy Sauce Eggs", 8), Card("Soy Sauce Eggs", 14), Card("Bok Choy"), Card("Tofu Ramen", 3), Card("Tonkatsu Ramen", 2), Card("Spicy Ramen", 1), Card("Spicy Ramen", 3, Card("Spicy Ramen", 7), Card("Spicy Ramen", 10))]
+    }
+    
+    // set addCard(Card()) {
+    //     deck.append(Card())
+    // }
+    get getDeck() {
+        return deck
+    }
+
+    dealPlayerHand(userHand) {
+        //at start of game deal 7 random cards to each User
+
+        for (let i = 0; i < 10; i++) {
+            let randIdx = Math.floor(Math.random() * deck.length)
+            let randC = deck.remove(randIdx)
+            userHand.append(randC)
+        }
+    } 
+}
+
+//Holds all exported functons/attributes
+module.exports.deck = this.deck;
+module.exports.dealPlayerHand = this.dealPlayerHand;
---
+++ client/src/Classes/User.js
@@ -0,0 +1,31 @@
+
+// const {
+//     deck,
+//     dealPlayerHand
+//     }    = require("./Deck.js");
+
+
+class User {
+    static d = deck()
+    constructor(userID, cardsToPick, hand, numPuddings, leftUser, rightUser) {
+        this.userID = userID;
+        this.cardsToPick = cardsToPick;
+        this.hand = d.dealPlayerHand(hand);
+        this.numPuddings = numPuddings;
+        this.leftUser = leftUser;
+        this.rightUser = rightUser;
+    }
+    
+    get leftUser() {
+        return this.leftUser
+    }
+
+    get rightUser() {
+        return this.rightUser
+    }
+ 
+    get hand() {
+        console.log(hand)
+        return hand
+    }
+}
