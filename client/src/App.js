db1f92a9f728598e92d6715a6f6539f0198cd279
tinan272 <mnguyen354@gatech.edu>
2023-03-06 18:40:20 -0500

    ChakraUI Modal attempt

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,18 @@ import Join from "./pages/Join"
 import NewCode from "./pages/NewCode"
 import Results from "./pages/Results";
 import '../src/style.css'
-
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Button,
+  ChakraProvider
+} from '@chakra-ui/react'
 
 export default function App() {
   const socket = io.connect("http://localhost:4000")
@@ -15,16 +26,36 @@ export default function App() {
   socket.on('other-test-channel', (data) => {
     console.log('received socket data from server:', JSON.stringify(data));
   });
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
-    <div className="App">
-      {/* <h1>Hello World</h1> */}
-      <Card />
-      <Game />
-      <Instructions />
-      <Join />
-      <NewCode />
-      <Results> </Results>
-    </div>
+    <ChakraProvider>
+      <div className="App">
+        {/* <h1>Hello World</h1> */}
+        <Card />
+        <Game />
+        <Button onClick={onOpen}>Open Modal</Button>
+        <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody>
+                This is where...
+            </ModalBody>
+            <ModalFooter>
+                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                Close
+                </Button>
+                <Button variant='ghost'>Secondary Action</Button>
+            </ModalFooter>
+            </ModalContent>
+        </Modal>
+        <Instructions />
+        <Join />
+        <NewCode />
+        <Results> </Results>
+      </div>
+    </ChakraProvider>
   );
 }
--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -3,44 +3,41 @@ import Card from "../components/Card.js"
 
 export default function Game() {
     return (
-        <div className="GamePage">
-            {/* <h1>Game</h1>
-            <Card />
-            <Card /> */}
-            <h1>Game</h1>
-            <div className="Card--container">
-                <div className="Card--setOne">
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+            <div className="GamePage">
+                <h1>Game</h1>
+                <div className="Card--container">
+                    <div className="Card--setOne">
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <div className="Card--deck">
+                            <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        </div>
                     </div>
-                </div>
-                <div className="Card--setTwo">
-                    <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                    <div className="Card--setTwo">
+                        <div className="Card--deck">
+                            <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        </div>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card id="deck2" className="Card--image" title="Deck 2" image="images/GameCards.png" value="14"/>
                     </div>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card id="deck2" className="Card--image" title="Deck 2" image="images/GameCards.png" value="14"/>
-                </div>
-                <div className="Card--setThree">
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
-                    </div>                </div>
-                <div className="Card--setFour">
-                    <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                    <div className="Card--setThree">
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <div className="Card--deck">
+                            <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        </div>                </div>
+                    <div className="Card--setFour">
+                        <div className="Card--deck">
+                            <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        </div>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                        <Card id="deck4" className="Card--image" name="Deck 4" image="images/GameCards.png" value="14"/>
                     </div>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card id="deck4" className="Card--image" name="Deck 4" image="images/GameCards.png" value="14"/>
                 </div>
             </div>
-        </div>
     )
 }
