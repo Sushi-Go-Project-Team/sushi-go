d09044c59ddc612bde5b58954b95b2cdfcccfd26
unknown <amytang325@gmail.com>
2023-03-28 18:29:01 -0400

    replaced temporary cards in game page

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -10,35 +10,35 @@ export default function Game() {
             <h1>Game</h1>
             <div className="Card--container">
                 <div className="Card--setOne">
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
                     <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        <Card name="Deck 1" image="images/card-back.png" value="14"/>   
                     </div>
                 </div>
                 <div className="Card--setTwo">
                     <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        <Card name="Deck 1" image="images/card-back.png" value="14"/>   
                     </div>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card id="deck2" className="Card--image" title="Deck 2" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card id="deck2" className="Card--image" title="Deck 2" image="images/card-back.png" value="14"/>
                 </div>
                 <div className="Card--setThree">
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
                     <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        <Card name="Deck 1" image="images/card-back.png" value="14"/>   
                     </div>                </div>
                 <div className="Card--setFour">
                     <div className="Card--deck">
-                        <Card name="Deck 1" image="images/GameCards.png" value="14"/>   
+                        <Card name="Deck 1" image="images/card-back.png" value="14"/>   
                     </div>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/GameCards.png" value="14"/>
-                    <Card id="deck4" className="Card--image" name="Deck 4" image="images/GameCards.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card className="Card--image" name="Soy Sauce Eggs" image="images/card-back.png" value="14"/>
+                    <Card id="deck4" className="Card--image" name="Deck 4" image="images/card-back.png" value="14"/>
                 </div>
             </div>
         </div>
