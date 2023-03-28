13547308097f6aff81c381f09d3e15ddd1016db3
unknown <amytang325@gmail.com>
2023-03-28 19:30:05 -0400

    join page change

--- client/src/pages/Join.js
+++ client/src/pages/Join.js
@@ -1,17 +1,14 @@
 import React from "react"
-import {Link} from "react-router-dom"
-import Button from "react-bootstrap/Button"
 
 export default function Join() {
     return (
         <div>
-	        <div className="center join">
-				<img src="images/join-logo.png" alt = "ramen go logo" className = "join-image"/>
+	        <div class="center join">
 		        <h2>Join Game</h2>
 	            <input type="text" placeholder="Game Pin"/>
-	            <button className="button">Enter</button>
+	            <button class= "button">Enter</button>
 	            <p>OR</p>
-	            <button className="button">Create New</button>
+	            <button class="button">Create New</button>
             </div>
         </div>
     )
