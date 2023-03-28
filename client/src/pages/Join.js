e2cd535536ffaec9edb2624ef67197f8dc915865
unknown <amytang325@gmail.com>
2023-03-28 19:02:23 -0400

    join page style

--- client/src/pages/Join.js
+++ client/src/pages/Join.js
@@ -3,12 +3,13 @@ import React from "react"
 export default function Join() {
     return (
         <div>
-	        <div class="center">
+	        <div className="center join">
+				<img src="images/join-logo.png" alt = "ramen go logo" className = "join-image"/>
 		        <h2>Join Game</h2>
 	            <input type="text" placeholder="Game Pin"/>
-	            <button class="button">Enter</button>
+	            <button className="button">Enter</button>
 	            <p>OR</p>
-	            <button class="button">Create New</button>
+	            <button className="button">Create New</button>
             </div>
         </div>
     )
