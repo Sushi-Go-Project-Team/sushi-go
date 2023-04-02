fd54b181f4f5c3521d76384bb26aa99bddbf93dd
unknown <amytang325@gmail.com>
2023-04-02 17:25:38 -0400

    styled results page

--- client/src/pages/Results.js
+++ client/src/pages/Results.js
@@ -1,20 +1,20 @@
 import React from "react"
 import {Link} from "react-router-dom"
 import Button from "react-bootstrap/Button"
+import ResultPlayerCard from "./ResultPlayerCard"
 
 export default function Results() {
     return (
         <div>
-            <h1 className = "Results">Results</h1>
+            <h1 className = "results">Results</h1>
             <link rel="stylesheet" type="text/css" href="style.css"/>
-            <body class = "Container">
-                <pre><p className = "Player">1 PLAYER                                      60</p></pre>
-                <pre><p className = "Player">2 OTHER PLAYER                                50</p></pre>
-                <pre><p className = "Player">3 OTHER PLAYER 2                              43</p></pre>
-                <pre><p className = "Player">4 LAST BUT NOT LEAST                          45</p> </pre>
-                <Button><Link to="/join" className = "button--link">Rematch!</Link></Button>
-                {/*<button className = "Rematch"> Rematch!</button>*/}
-            </body>
+            <div class = "container">
+                <ResultPlayerCard number = "1" name="Player 1" score="60"/>
+                <ResultPlayerCard number = "2" name="Other Player" score="50"/>
+                <ResultPlayerCard number = "3" name="Another Player" score="45"/>
+                <ResultPlayerCard number = "4" name="Last But Not Least 4" score="43"/>
+                <Button className = "rematch"><Link to="/join" >Rematch!</Link></Button>
+            </div>
         </div>
     )
 }
