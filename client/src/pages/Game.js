ec2e3d643897dcc3ec95e11208983cbf8bb71c93
Amy <45153739+amtang325@users.noreply.github.com>
2023-04-18 12:36:32 -0400

    game page styling

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -11,7 +11,7 @@ export default function Game({socket, user, users, endGame, pickCard, setPlayer,
     let otherName;
     for (let i = 0; i < users.length; i++) {
         if (users[i].name !== user.name) {
-            otherHand = users[i].cardsPicked;
+            otherHand = users[i].currentHand;
             otherName = users[i].name;
             break;
         }
@@ -81,8 +81,7 @@ export default function Game({socket, user, users, endGame, pickCard, setPlayer,
 
     return (
             <div className="GamePage">
-                <h1>Game</h1>
-                <button className="openModalBtn" onClick={() => {
+                <button className="openModalBtn modalButton" onClick={() => {
                     setOpenModal(true)
                     }}
                     >
@@ -100,15 +99,15 @@ export default function Game({socket, user, users, endGame, pickCard, setPlayer,
         socket={socket}
         code = {code} />}
                 <div className="Card--container">
-                <div className="Card--setOne">
-                    <h2>{user.name}</h2>
+                <div className="card--set card--set-1">
+                    <h2>{user.name}'s Cards</h2>
                     {pickedCards}
                     {/* <div className="Card--deck">
                         <Card name="Deck 1" image="images/card-back.png" value="14"/>   
                     </div> */}
                 </div>
-                <div className="Card--setTwo">
-                    <h2>{otherName}</h2>
+                <div className="card--set">
+                    <h2>{otherName}'s Cards</h2>
                     {otherCards}
                     {/* <div className="Card--deck">
                         <Card name="Deck 1" image="images/card-back.png" value="14"/>   
@@ -133,7 +132,7 @@ export default function Game({socket, user, users, endGame, pickCard, setPlayer,
                     <Card id="deck4" className="Card--image" name="Deck 4" image="images/card-back.png" value="14"/>
                 </div> */}
             </div>
-            <button onClick={endGame}><Link to="/results" className = "button--link">End Game!</Link></button>
+            <button className="game-button" onClick={endGame}><Link to="/results" className = "button--link">End Game!</Link></button>
             </div>
     )
 }
